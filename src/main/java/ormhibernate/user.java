759e4bd46ea06a6cb71e1a88c6c1738193d09608
JUANCARLOS-222 <juancarlosferrer.2003@gmail.com>
2024-04-11 18:58:06 +0200

    Primer Commit

---
+++ src/main/java/ormhibernate/user.java
@@ -0,0 +1,51 @@
+package ormhibernate;
+
+import javax.persistence.*;
+
+public class user {
+
+	String nombre;
+	String apellido;
+	int numeroTelefono;
+	String email;
+	
+	public user(String nombre, String apellido, int numeroTelefono, String email ) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.numeroTelefono = numeroTelefono;
+		this.email = email;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public int getNumeroTelefono() {
+		return numeroTelefono;
+	}
+	
+	public void setNumeroTelefono(int numeroTelefono) {
+		this.numeroTelefono = numeroTelefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
